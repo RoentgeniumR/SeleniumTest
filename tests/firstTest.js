ebad44e70e7569d2902f4ef26b32f9e35a689f43
Rontgen <rrsr4371@gmail.com>
2024-02-23 21:52:38 +0800

    first commit

---
+++ tests/firstTest.js
@@ -0,0 +1,13 @@
+const { Builder, By, Key } = require("selenium-webdriver");
+
+async function example() {
+
+    let driver = await new Builder().forBrowser("MicrosoftEdge").build();
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+    await driver.quit();
+
+}
+example()
